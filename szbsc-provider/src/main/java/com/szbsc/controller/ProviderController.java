7d41e971451f1c28969faa67a1101a898bffb4b5
guozhidong <zhidong.guo@cnbaosi.com>
2022-09-23 17:39:54 +0800

    gateway

--- szbsc-provider/src/main/java/com/szbsc/controller/ProviderController.java
+++ szbsc-provider/src/main/java/com/szbsc/controller/ProviderController.java
@@ -1,5 +1,6 @@
 package com.szbsc.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,8 +9,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/provider")
 public class ProviderController {
 	
+	@Value("${server.port}")
+	private Integer port;
+	
 	@RequestMapping("/hello")
 	public String test(@RequestParam String name) {
-		return "hello, " + name;
+		System.err.println("Say hello to " + name + " from port [" + this.port + "]");
+		return "Say hello to " + name + " from port [" + this.port + "]";
 	}
 }
